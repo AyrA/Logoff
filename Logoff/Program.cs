446856de043de9ede81c3bcd2f5784f44a6adf3f
AyrA <admin@ayra.ch>
2020-08-22 01:59:21 +0200

    Add project files.

---
+++ Logoff/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Logoff
+{
+    class Program
+    {
+        private static extern bool ExitWindowsEx(int i, int Y);
+
+        static void Main(string[] args)
+        {
+        }
+    }
+}
